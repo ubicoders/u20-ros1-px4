7fae4578f14625dba08daf5cf65d31d1eb8a1715
el <test@ubicoders.com>
2024-02-06 21:17:57 -0800

    aa

--- download_px4_autopilot.bash
+++ download_px4_autopilot.bash
@@ -3,10 +3,10 @@ pip3 install jsonschema
 cd /home/ubuntu
 git clone https://github.com/PX4/PX4-Autopilot --recursive
 cd PX4-Autopilot
-git checkout v1.14.0 # tag at release/1.14.0 branch
+git checkout -f v1.14.0 # tag at release/1.14.0 branch
 git submodule update --recursive
 git submodule update --init --recursive
 
 cd /home/ubuntu
 git clone https://github.com/mavlink/mavlink.git --recursive
-PYTHONPATH=/home/user/mavlink
+PYTHONPATH=/home/ubuntu/mavlink
